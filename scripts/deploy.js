cf0bec10580e3283f024eaa6774bb0447eb9813e
alfahim-bs23 <al.fahim@brainstation-23.com>
2025-04-24 18:12:59 +0600

    feat: contract interaction,verification added

---
+++ scripts/deploy.js
@@ -0,0 +1,46 @@
+const {ethers,run,network}=require("hardhat");
+
+async function main() {
+    const Contract = await ethers.getContractFactory("SimpleStorage");
+    const contract = await Contract.deploy();
+    await contract.waitForDeployment();
+    const address=await contract.getAddress();
+
+    if(network.config.chainId==11155111){
+        console.log("waitting for verification...");
+        await contract.deploymentTransaction().wait(6);
+        await verify(address,[]);
+    }
+    console.log("Contract deployed to:", address);
+
+    const curVal=await contract.retrieve();
+    console.log("cur val: ",curVal);
+    await contract.store(33);
+    const updatedVal=await contract.retrieve();
+    console.log("updated val: ",updatedVal);
+
+}
+
+
+const verify=async(contractAddress,args)=>{
+    console.log("Verifying contract");
+    try{
+        await run("verify:verify",{
+            address:contractAddress,
+            constructorArguments:args
+        })
+    }catch(e){
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+.then(()=>process.exit(0))
+.catch((error)=>{
+    console.log(error);
+    process.exit(1);
+})
